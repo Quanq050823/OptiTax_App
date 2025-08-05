24f965e380fad9e0272b081ae56231bbd2ade97e
Tran Quang <21110793@student.hcmute.edu.vn>
2025-08-06 00:16:09 +0700

    fix(project): Remove wrong front-end source code

--- src/presentation/Hooks/useAppNavigation.tsx
+++
@@ -1,5 +0,0 @@
-import { NavigationProp, useNavigation } from "@react-navigation/native";
-
-export const useAppNavigation = () => {
-  return useNavigation<NavigationProp<RootStackParamList>>();
-};
--- src/presentation/components/colors/index.tsx
+++
@@ -1,3 +0,0 @@
-export const ColorMain = "#3F4E87";
-export const themeBackground = "#9d9d9d";
-export const iconNavigationColor = "#0b2177ff"
