e7aea309d52454ab22217c2abddb71089cfe5675
Tran Quang <21110793@student.hcmute.edu.vn>
2025-08-08 01:18:22 +0700

    refactor(backend/model): Clean code

--- server/src/services/authService.js
+++ server/src/services/authService.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import User from "./../models/user.js";
+import User from "./../models/User.js";
 import bcrypt from "bcrypt";
 import ApiError from "../utils/ApiError.js";
 import { StatusCodes } from "http-status-codes";
