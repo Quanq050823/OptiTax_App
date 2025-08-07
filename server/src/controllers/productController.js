40deef81fe983582e68282fba21f1a3647034b11
Tran Quang <21110793@student.hcmute.edu.vn>
2025-08-07 16:12:41 +0700

    feat(backend/product): create CURD API for business product management

---
+++ server/src/controllers/productController.js
@@ -0,0 +1,89 @@
+"use strict";
+
+import * as productService from "../services/productService.js";
+import { StatusCodes } from "http-status-codes";
+import Product from "../models/Product.js";
+import BusinessOwner from "../models/BusinessOwner.js";
+
+const create = async (req, res, next) => {
+	try {
+		const userId = req.user.userId;
+
+		const owner = await BusinessOwner.findOne({ userId });
+		if (!owner)
+			return res.status(404).json({ message: "BusinessOwner not found" });
+
+		const product = await Product.create({
+			...req.body,
+			ownerId: owner._id,
+		});
+
+		res.status(StatusCodes.CREATED).json(product);
+	} catch (err) {
+		next(err);
+	}
+};
+
+const getById = async (req, res, next) => {
+	try {
+		const id = req.params.id;
+		const result = await productService.getProductById(id);
+		res.status(StatusCodes.OK).json(result);
+	} catch (err) {
+		next(err);
+	}
+};
+
+const update = async (req, res, next) => {
+	try {
+		const id = req.params.id;
+		const data = req.body;
+		const result = await productService.updateProduct(id, data);
+		res.status(StatusCodes.OK).json(result);
+	} catch (err) {
+		next(err);
+	}
+};
+
+const remove = async (req, res, next) => {
+	try {
+		const id = req.params.id;
+		const result = await productService.deleteProduct(id);
+		res.status(StatusCodes.OK).json(result);
+	} catch (err) {
+		next(err);
+	}
+};
+
+const list = async (req, res, next) => {
+	try {
+		const ownerId = req.user.userId;
+		const { page, limit, sortBy, sortOrder, ...filter } = req.query;
+		const options = {
+			page: parseInt(page) || 1,
+			limit: parseInt(limit) || 10,
+			sortBy: sortBy || "createdAt",
+			sortOrder: parseInt(sortOrder) || -1,
+		};
+		const result = await productService.listProducts(ownerId, filter, options);
+		res.status(StatusCodes.OK).json(result);
+	} catch (err) {
+		next(err);
+	}
+};
+
+const listMyProducts = async (req, res, next) => {
+	try {
+		const userId = req.user.userId;
+		const owner = await BusinessOwner.findOne({ userId });
+		if (!owner)
+			return res.status(404).json({ message: "BusinessOwner not found" });
+
+		const products = await Product.find({ ownerId: owner._id });
+		res.status(200).json(products);
+	} catch (err) {
+		next(err);
+	}
+};
+
+export { create, getById, update, remove, list, listMyProducts };
