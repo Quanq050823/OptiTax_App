39f9ee666aea81f6bee8e3341593a8077edb37af
khang03 <nguyenthekhang2210@gmail.com>
2025-08-13 10:05:04 +0700

    err database edit

--- server/src/config/mongodb.js
+++ server/src/config/mongodb.js
@@ -5,23 +5,23 @@ import config from "./environment.js";
 import chalk from "chalk";
 
 const db = async () => {
-	try {
-		await mongoose.connect(config.connStr);
-		console.log(chalk.blueBright("Connected to MongoDB Atlas"));
-	} catch (error) {
-		console.error("Error connecting to the database:", err);
-		process.exit(1);
-	}
+  try {
+    await mongoose.connect(config.connStr);
+    console.log(chalk.blueBright("Connected to MongoDB Atlas"));
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    process.exit(1);
+  }
 };
 
 const closeConnection = async () => {
-	try {
-		await mongoose.connection.close();
-		console.log(chalk.red("MongoDB Atlas connection closed"));
-	} catch (error) {
-		console.error("Error closing connection:", err);
-		throw new Error(error);
-	}
+  try {
+    await mongoose.connection.close();
+    console.log(chalk.red("MongoDB Atlas connection closed"));
+  } catch (error) {
+    console.error("Error closing connection:", error);
+    throw new Error(error);
+  }
 };
 
 export { db, closeConnection };
