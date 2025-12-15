4ae274af3fe849d84dad12461eb354ba7f962ae1
Tran Quang <21110793@student.hcmute.edu.vn>
2025-12-16 00:27:27 +0700

    spike easyInvoice & fix captcha img

--- Client/src/services/API/axios.tsx
+++ Client/src/services/API/axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { TokenStorage } from "../../utils/tokenStorage";
 
-const BASE_URL = "http://192.168.1.35:3001/api/";
+const BASE_URL = "http://192.168.1.46:3001/api/";
 
 const axiosInstance = axios.create({
 	baseURL: BASE_URL,
