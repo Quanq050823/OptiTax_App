744dfe76b4621e30769de1dc303167509678df4a
Tran Quang <21110793@student.hcmute.edu.vn>
2026-03-28 18:27:14 +0700

    delete server code

--- server/src/models/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-import User from "./user.js";
-import BusinessOwner from "./BusinessOwner.js";
-import Accountant from "./Accountant.js";
-import Customer from "./Customer.js";
-import TaxSubmission from "./TaxSubmission.js";
-
-export { User, BusinessOwner, Accountant, Customer, TaxSubmission };
