0e56ffea83955246aa21ef82ec48afdb67935bde
Duc Quang <quang.tran.duc@metagrit.asia>
2026-06-22 17:00:30 +0700

    update release

--- Client/src/services/API/axios.tsx
+++ Client/src/services/API/axios.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { TokenStorage } from "../../utils/tokenStorage";
 
-const BASE_URL = "https://eonapp.duckdns.org/api/";
+const BASE_URL = "https://dattax.duckdns.org/api/";
+
 // const BASE_URL = "http://172.16.0.108:3001/api/";
 
 const axiosInstance = axios.create({
